ef52121eea8f80275d401ade9f465a2454b16839
timeades <tim.eades@mac.com>
2023-01-12 16:45:45 +0000

    add helper loginComponent

---
+++ tests/utils/helpers/loginComponents.ts
@@ -0,0 +1,9 @@
+export enum LogInComponent {
+    emailInput = '[data-qa-id="email-input"]',
+    passwordInput = '[data-qa-id="password-input"]',
+    logIn = '[data-qa-id="login-btn"]',
+    rememberMe = 'data-qa-id="remember-me-checkbox-label"]',
+    needHelp = '[data-qa-id="need-help-link"]',
+    errorDisplay = '[data-qa-id="error-display"]',
+    orgLogIn = '[data-qa-id="log-in-with-organization-btn"]'
+}
